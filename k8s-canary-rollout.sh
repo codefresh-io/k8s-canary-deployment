46b8bb75bd72628de02e6e113532f38ced927383
Ryan Smith <rms1000watt@gmail.com>
2018-12-11 16:13:18 -0800

    added HPA handling and bugfixes with input deployment

--- k8s-canary-rollout.sh
+++ k8s-canary-rollout.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 
 healthcheck(){
     echo "[CANARY INFO] Starting Heathcheck"
     h=true
-    
+
     #Start custom healthcheck
     output=$(kubectl get pods -l app="$DEPLOYMENT_NAME" -n $NAMESPACE --no-headers)
     echo "[CANARY HEALTH] $output"
@@ -29,31 +28,37 @@ healthcheck(){
 
 cancel(){
     echo "[CANARY] Cancelling rollout - healthcheck failed"
-    
+
     echo "[CANARY SCALE] Restoring original deployment to $PROD_DEPLOYMENT"
     kubectl apply -f $WORKING_VOLUME/original_deployment.yaml -n $NAMESPACE
     kubectl rollout status deployment/$PROD_DEPLOYMENT
 
     #we could also just scale to 0.
-    echo "[CANARY DELETE] Removing canary completely"
+    echo "[CANARY DELETE] Removing canary deployment completely"
     kubectl delete deployment $CANARY_DEPLOYMENT
+
+    echo "[CANARY DELETE] Removing canary horizontal pod autoscaler completely"
+    kubectl delete deployment $CANARY_DEPLOYMENT -n $NAMESPACE
 
     exit 1
 }
 
-
 cleanup(){
     echo "[CANARY CLEANUP] removing previous deployment $PROD_DEPLOYMENT"
     kubectl delete deployment $PROD_DEPLOYMENT -n $NAMESPACE
-    echo "[CANARY CLEANUP] marking canary as new production"
-    kubectl get service $SERVICE_NAME -o=yaml --namespace=${NAMESPACE} | sed -e "s/$CURRENT_VERSION/$NEW_VERSION/g" | kubectl apply --namespace=${NAMESPACE} -f - 
 
+    echo "[CANARY CLEANUP] removing previous horizontal pod autoscaler $PROD_DEPLOYMENT"
+    kubectl delete hpa $PROD_DEPLOYMENT -n $NAMESPACE
+
+    echo "[CANARY CLEANUP] marking canary as new production"
+    kubectl get service $SERVICE_NAME -o=yaml --namespace=${NAMESPACE} | sed -e "s/$CURRENT_VERSION/$NEW_VERSION/g" | kubectl apply --namespace=${NAMESPACE} -f -
 }
+
 incrementservice(){
     percent=$1
     starting_replicas=$2
-    
-    #debug 
+
+    #debug
     #echo "Increasing canaries to $percent percent, max replicas is $starting_replicas"
 
     prod_replicas=$(kubectl get deployment $PROD_DEPLOYMENT -n $NAMESPACE -o=jsonpath='{.spec.replicas}')
@@ -79,17 +84,16 @@ incrementservice(){
     fi
 
     echo "[CANARY SCALE] Setting canary replicas to $new_canary_replicas"
-    kubectl -n $NAMESPACE scale --replicas=$new_canary_replicas deploy/$CANARY_DEPLOYMENT 
+    kubectl -n $NAMESPACE scale --replicas=$new_canary_replicas deploy/$CANARY_DEPLOYMENT
 
     echo "[CANARY SCALE] Setting production replicas to $new_prod_replicas"
-    kubectl -n $NAMESPACE scale --replicas=$new_prod_replicas deploy/$PROD_DEPLOYMENT 
-
+    kubectl -n $NAMESPACE scale --replicas=$new_prod_replicas deploy/$PROD_DEPLOYMENT
 
     #Wait a bit until production instances are down. This should always succeed
     kubectl -n $NAMESPACE rollout status deployment/$PROD_DEPLOYMENT
 
     #Calulate increments. N = the number of starting pods, I = Increment value, X = how many pods to add
-    # x / (N + x) = I 
+    # x / (N + x) = I
     # Starting pods N = 5
     # Desired increment I = 0.35
     # Solve for X
@@ -106,7 +110,6 @@ incrementservice(){
     # 1			5	3
     # 2			2	6
     # 3			0	5
-
 }
 
 copy_deployment(){
@@ -117,12 +120,7 @@ copy_deployment(){
     #Replace docker image
     sed -Ei -- "s/$CURRENT_VERSION/$NEW_VERSION/g" $WORKING_VOLUME/canary_deployment.yaml
     echo "[CANARY INFO] Replaced image name"
-
-
     echo "[CANARY INFO] Production deployment is $PROD_DEPLOYMENT, canary is $CANARY_DEPLOYMENT"
-    #Start with one replica
-    sed -Ei -- "s#replicas: $STARTING_REPLICAS#replicas: 1#g" $WORKING_VOLUME/canary_deployment.yaml
-    echo "[CANARY INIT] Launching 1 pod with canary"
 }
 
 input_deployment(){
@@ -131,21 +129,20 @@ input_deployment(){
 }
 
 mainloop(){
-
     echo "[CANARY INFO] Selecting Kubernetes cluster"
     kubectl config use-context ${KUBE_CONTEXT}
 
     echo "[CANARY INFO] Locating current version"
-    CURRENT_VERSION=$(kubectl get service $SERVICE_NAME -o=jsonpath='{.metadata.labels.version}' --namespace=${NAMESPACE}) 
+    CURRENT_VERSION=$(kubectl get service $SERVICE_NAME -o=jsonpath='{.metadata.labels.version}' --namespace=${NAMESPACE})
 
     if [ "$CURRENT_VERSION" == "$NEW_VERSION" ]; then
        echo "[DEPLOY NOP] NEW_VERSION is same as CURRENT_VERSION. Both are at $CURRENT_VERSION"
        exit 0
-    fi  
-    
+    fi
+
     echo "[CANARY INFO] current version is $CURRENT_VERSION"
     PROD_DEPLOYMENT=$DEPLOYMENT_NAME-$CURRENT_VERSION
-   
+
     echo "[CANARY INFO] Locating current deployment"
     kubectl get deployment $PROD_DEPLOYMENT -n $NAMESPACE -o=yaml > $WORKING_VOLUME/canary_deployment.yaml
 
@@ -156,20 +153,32 @@ mainloop(){
     IMAGE=$(kubectl get deployment $PROD_DEPLOYMENT -n $NAMESPACE -o=yaml | grep image: | sed -E 's/.*image: (.*)/\1/')
     echo "[CANARY INFO] found image $IMAGE"
     echo "[CANARY INFO] Finding current replicas"
-    STARTING_REPLICAS=$(kubectl get deployment $PROD_DEPLOYMENT -n $NAMESPACE --no-headers | awk '{print $2}')
-    echo "[CANARY INFO] Found replicas $STARTING_REPLICAS"
 
     if [[ -n ${INPUT_DEPLOYMENT} ]]; then
         #Allow user to provide custom new yaml deployment object
         input_deployment
+
+        if ! STARTING_REPLICAS=$(grep "replicas:" < ${WORKING_VOLUME}/canary_deployment.yaml | awk '{print $2}'); then
+            echo "[CANARY INFO] Failed getting replicas from input file: ${WORKING_VOLUME}/canary_deployment.yaml"
+            echo "[CANARY INFO] Using the same number of replicas from prod deployment"
+            STARTING_REPLICAS=$(kubectl get deployment $PROD_DEPLOYMENT -n $NAMESPACE -o=jsonpath='{.spec.replicas}')
+        fi
     else
         #Copy existing deployment and update image only
         copy_deployment
+
+        STARTING_REPLICAS=$(kubectl get deployment $PROD_DEPLOYMENT -n $NAMESPACE -o=jsonpath='{.spec.replicas}')
+        echo "[CANARY INFO] Found replicas $STARTING_REPLICAS"
     fi
+
+    #Start with one replica
+    sed -Ei -- "s#replicas: $STARTING_REPLICAS#replicas: 1#g" $WORKING_VOLUME/canary_deployment.yaml
+    echo "[CANARY INIT] Launching 1 pod with canary"
 
     #Launch canary
     kubectl apply -f $WORKING_VOLUME/canary_deployment.yaml -n $NAMESPACE
 
+    echo "[CANARY INFO] Canary target replicas: $STARTING_REPLICAS"
 
     healthcheck
 
@@ -180,7 +189,7 @@ mainloop(){
             p=100
         fi
         echo "[CANARY INFO] Rollout is at $p percent"
-        
+
         incrementservice $TRAFFIC_INCREMENT $STARTING_REPLICAS
 
         if [ "$p" == "100" ]; then
@@ -192,9 +201,6 @@ mainloop(){
         sleep $SLEEP_SECONDS
      	healthcheck
     done
-
-
-
 }
 
 if [ "$1" != "" ] && [ "$2" != "" ] && [ "$3" != "" ] && [ "$4" != "" ] && [ "$5" != "" ] && [ "$6" != "" ] && [ "$7" != "" ]; then
@@ -207,7 +213,6 @@ if [ "$1" != "" ] && [ "$2" != "" ] && [ "$3" != "" ] && [ "$4" != "" ] && [ "$5
     SLEEP_SECONDS=$7
     CANARY_DEPLOYMENT=$DEPLOYMENT_NAME-$NEW_VERSION
 else
-    
     echo "USAGE\n k8s-canary-rollout.sh [WORKING_VOLUME] [SERVICE_NAME] [DEPLOYMENT_NAME] [TRAFFIC_INCREMENT] [NAMESPACE] [NEW_VERSION] [SLEEP_SECONDS]"
     echo "\t [WORKING_VOLUME] - This should be set with \${{CF_VOLUME_PATH}}"
     echo "\t [SERVICE_NAME] - Name of the current service"
@@ -222,4 +227,3 @@ fi
 echo $BASH_VERSION
 
 mainloop
-
